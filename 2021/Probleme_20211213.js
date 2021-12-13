010fc172a870d1458f2a8c31262d2edbbb0d3915
JosquinH <josquin.havard@orange.fr>
2021-12-13 13:52:56 +0100

    :sparkles: Avancement Problème 13

--- 2021/Probleme_20211213.js
+++ 2021/Probleme_20211213.js
@@ -2,10 +2,7 @@ const {dots, instructions} = require('./input/input_test')
 
 // Formattage
 
-const minX = 0
 let maxX = 0
-
-const minY = 0
 let maxY = 0
 
 const dots2 = []
@@ -20,9 +17,9 @@ for (const dot of dots) {
 
 const newInput = []
 
-for (let i = 0; i < maxY; ++ i) {
+for (let i = 0; i <= maxY; ++ i) {
     let curInput = []
-    for (let j = 0; j < maxX; ++j) {
+    for (let j = 0; j <= maxX; ++j) {
         curInput.push('.')
     }
     newInput.push(curInput)
@@ -32,6 +29,56 @@ for (const dot of dots2) {
     newInput[dot[0]][dot[1]] = '#'
 }
 
-for (const t of newInput) {
+// 1
+
+let ancientInput = newInput
+
+for (const instruction of instructions) {
+    for (const t of ancientInput) {
+        console.log(t.join(''))
+    }
+
+    console.log('\n')
+    
+    const axe = instruction.split('=')[0]
+    const axeVal = parseInt(instruction.split('=')[1])
+    
+    const curNewInput = []
+
+    if (axe === 'y') {
+        const y1 = axeVal - 1
+        const y2 = axeVal + 1
+        for (let i = y1; i >= 0; --i) {
+            const curLine = []
+            const line1 = ancientInput[y1 - i]
+            const line2 = ancientInput[y2 + i]
+            for (let j = 0; j < line1.length; ++j) {
+                if (line1[j] === '#' || line2[j] === '#') {
+                    curLine.push('#')
+                } else {
+                    curLine.push('.')
+                }
+            }
+            curNewInput.push(curLine)
+        }
+    } else if (axe === 'x') {
+        const x1 = axeVal - 1
+        const x2 = axeVal + 1
+        for (const line of ancientInput) {
+            const curLine = []
+            for (let i = x1; i >= 0; --i) {
+                if (line[x1 - i] === '#' || line[x2 + i] === '#') {
+                    curLine.push('#')
+                } else {
+                    curLine.push('.')
+                }
+            }
+            curNewInput.push(curLine)
+        }
+    }
+    ancientInput = curNewInput
+}
+
+for (const t of ancientInput) {
     console.log(t.join(''))
 }
