e12a8ab9968f10d4b0bda5c53d396deb7472fb77
JosquinH <josquin.havard@orange.fr>
2021-12-14 09:50:38 +0100

    :sparkles: Suite problème 14

--- 2021/Probleme_20211214.js
+++ 2021/Probleme_20211214.js
@@ -1,4 +1,4 @@
-const {template, pairInsertions} = require('./input/input_test')
+const {template, pairInsertions} = require('./input/input_20211214')
 
 // Reformattage
 
@@ -13,17 +13,41 @@ for (const pair of pairInsertions) {
 
 let ancientTemplate = template.split('')
 let step = 0
-const numberOfStep = 10
+
+let template1 = []
+const numberOfStep = 40
 
 while (step < numberOfStep) {
+    console.log(`step:${step}`)
+    console.log(ancientTemplate.length)
     const newTemplate = [ancientTemplate[0]]
     for (let i = 0; i < ancientTemplate.length - 1; ++i) {
         newTemplate.push(pairInsertionsObj[`${ancientTemplate[i]}${ancientTemplate[i+1]}`])
         newTemplate.push(ancientTemplate[i+1])
     }
     ancientTemplate = newTemplate
+    if (step === 10) {
+        template1 = ancientTemplate
+    }
     ++step
 }
 
-let objOccur = {}
+const objOccur1 = {}
+
+for (let i = 0; i < template1.length; ++ i) {
+    const c = template1[i]
+    if (objOccur1[c]) {
+        objOccur1[c] += 1
+    } else {
+        objOccur1[c] = 1
+    }
+}
+
+const occurences = Object.values(objOccur)
+
+occurences.sort((a,b) => a - b)
+
+console.log(`1st question's answer : ${occurences[occurences.length - 1] - occurences[0]}`)
+
+// 2
 
