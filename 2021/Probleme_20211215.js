c2731f77cadf1069071911de3e4710148fc54895
JosquinH <josquin.havard@orange.fr>
2021-12-15 15:37:04 +0100

    :sparkles: début prob 15

--- 2021/Probleme_20211215.js
+++ 2021/Probleme_20211215.js
@@ -1,3 +1,35 @@
-const input = require('./input/input_20211215')
+const input = require('./input/input_test')
 
-console.log(input.length * input[0].length)
+// Reformattage
+
+const newInput = []
+
+for (const curInput of input) {
+    newInput.push(curInput.split('').map(x => parseInt(x)))
+}
+
+// construction du graphe
+
+const INPUT_HEIGHT = newInput.length
+const INPUT_WIDTH = newInput[0].length
+const graphe = {}
+for (let i = 0; i < INPUT_HEIGHT; ++i) {
+    for (let j = 0; j < INPUT_WIDTH; ++j) {
+        const adjacenceList = []
+        if (i > 0) {
+            adjacenceList.push([`${i-1};${j}`, newInput[i-1][j]])
+        }
+        if (i< INPUT_HEIGHT - 1) {
+            adjacenceList.push([`${i+1};${j}`, newInput[i+1][j]])
+        }
+        if (j > 0) {
+            adjacenceList.push([`${i};${j-1}`, newInput[i][j-1]])
+        }
+        if (j< INPUT_WIDTH - 1) {
+            adjacenceList.push([`${i};${j+1}`, newInput[i][j+1]])
+        }
+        graphe[`${i};${j}`] = adjacenceList
+    }
+}
+
+console.log(graphe)
