d0845bf3555796af11c7eb05960ad2cb512013e1
JosquinH <josquin.havard@orange.fr>
2021-12-20 11:44:29 +0100

    :sparkle: Problème 20

--- 2021/Probleme_20211220.js
+++ 2021/Probleme_20211220.js
@@ -1,9 +1,9 @@
-const {ehancementString, image} = require('./input/input_test2')
+const {ehancementString, image} = require('./input/input_20211220')
 
 const ORIGINAL_IMAGE_WIDTH = image.length
 const ORIGINAL_IMAGE_HEIGHT = image[0].length
 const NUMBER_OF_STEP = 2
-
+const ehancementStringTable = ehancementString.split('')
 let dotString = ''
 let j = 0
 
@@ -68,7 +68,11 @@ for (let step = 1; step <= NUMBER_OF_STEP; ++step) {
             str += newImage[i + 1][j] === '#' ? '1' : '0'
             str += newImage[i + 1][j+1] === '#' ? '1' : '0'
             const curIndex = fromBinToDec(str)
-            curLine.push(ehancementString.charAt(curIndex))
+            curLine.push(ehancementStringTable[curIndex])
+           /*  for (let k = i - 1; k <= i + 1 ; ++k) {
+                console.log (newImage[k].slice(j-1, j + 2).join(''))
+            } */
+            //console.log(curIndex)
         }
         newCurImage.push(curLine)
     }
@@ -76,7 +80,7 @@ for (let step = 1; step <= NUMBER_OF_STEP; ++step) {
 
     for (let i = newImageStartHeight - step; i < newImageEndHeight + step; ++i ) {
         for (let j = newImageStartWidth - step; j < newImageEndWidth + step; ++j ) {
-            newImage[i][j] = newCurImage[i - (newImageStartHeight - step)][j - (newImageStartWidth - step)]
+            newImage[i][j] = newCurImage[i - newImageStartHeight + step][j - newImageStartWidth + step]
         }
     }
 
