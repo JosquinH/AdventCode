2d2838eee822b86a08a5cb1a053f9edc17e6750c
JosquinH <josquin.havard@orange.fr>
2023-12-06 14:14:24 +0100

    CHangement loop itérateur

--- 2023/Problème_20231206.js
+++ 2023/Problème_20231206.js
@@ -8,26 +8,24 @@ const reg = /\d+/g
 
 const times = []
 const timesIt = input[0].matchAll(reg)
-let t = timesIt.next()
-while (!t.done) {
+let t
+while ((t = timesIt.next()).value !== undefined) {
     times.push(parseInt(t.value))
-    t = timesIt.next()
 }
 
 // Récupération distances
 
 const distances = []
 const distancesIt = input[1].matchAll(reg)
-let d = distancesIt.next()
-
-while (!d.done) {
+let d
+while ((d = distancesIt.next()).value !== undefined) {
     distances.push(parseInt(d.value))
-    d = distancesIt.next()
 }
 
+console.log(distances)
 /**
  * Le problème à résoudre est (time - x) * x > distance où x est le temps où l'on appuie sur le bouton
- * Cela se rammène à -(x**2) + time * x - distance > 0
+ * Cela se rammène à -(x**2) + time*x - distance > 0
  */
 
 const getNumberOfSolutions = (time,distance) => {
@@ -36,10 +34,10 @@ const getNumberOfSolutions = (time,distance) => {
     const c = -distance
     const deltaSqrt = Math.sqrt((b ** 2) - (4*a*c))
     
-    let x1 = (-1 * b + deltaSqrt) / (2*a)
-    x1 = Math.ceil(x1) === x1 ? x1 + 1 : Math.ceil(x1) // arrondi supérieur car valeur minimal
+    let x1 = (-b + deltaSqrt) / (2*a)
+    x1 = Math.ceil(x1) === x1 ? x1 + 1 : Math.ceil(x1) // arrondi supérieur car valeur min
     
-    let x2 = (-1 * b - deltaSqrt) / (2*a) 
+    let x2 = (-b - deltaSqrt) / (2*a) 
     x2 = Math.floor(x2) === x2 ? x2 - 1 : Math.floor(x2) // arrondi inférieur car valeur max
 
     return x2 - x1 + 1
