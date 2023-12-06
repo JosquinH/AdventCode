beeb0dbe2352d77b698d3f36e6147703709582bc
JosquinH <josquin.havard@orange.fr>
2023-12-06 09:41:25 +0100

    Ajout traitement input

--- 2023/Problème_20231206.js
+++ 2023/Problème_20231206.js
@@ -1,5 +1,27 @@
-const times = [42,89,91,89]
-const distances = [308,1170,1291,1467]
+const fs = require('fs');
+const filename = "input/input_20231206.txt"
+const input = fs.readFileSync(filename, 'utf8').split('\r\n')
+
+const reg = new RegExp('[0-9]+','g')
+
+// Récupération times
+
+const times = []
+const timesIt = input[0].matchAll(reg)
+let t = timesIt.next()
+while (!t.done) {
+    times.push(parseInt(t.value))
+    t = timesIt.next()
+}
+
+// Récupération distances
+const distances = []
+const distancesIt = input[1].matchAll(reg)
+let d = distancesIt.next()
+while (!d.done) {
+    distances.push(parseInt(d.value))
+    d = distancesIt.next()
+}
 
 // Problème 1
 
